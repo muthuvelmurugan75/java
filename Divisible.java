504a54225160b5f87dde12463e7dac15019e300a
muthuvelmurugan75 <muthuvelmurugan75@gmail.com>
2017-02-10 19:24:17 +0530

    Add files via upload

---
+++ Divisible.java
@@ -0,0 +1,18 @@
+package com.payilagam.www;
+
+public class Divisible {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int sum=0,a=0;
+		
+		for(int i=2;i<=8;i++){
+			if(i%2==0){
+			sum=sum+i;
+			
+			System.out.println(sum);
+			}
+		}
+		
+	}
+}
