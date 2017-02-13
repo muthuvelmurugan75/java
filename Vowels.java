6134285cef5e27add6258b93439c0cfa954bb6a7
muthuvelmurugan75 <muthuvelmurugan75@gmail.com>
2017-02-13 15:15:40 +0530

    Add files via upload

--- Vowels.java
+++ Vowels.java
@@ -5,15 +5,40 @@ import java.util.Scanner;
 public class Vowels {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner in=new Scanner(System.in);
-		int a=in.nextInt();
-		int b=in.nextInt();
-		a=a+b;
-		b=a-b;
-		a=a-b;
-		System.out.println(a);
-		System.out.println(b);
-	}
+		System.out.println("enter a name");
+		String str=in.nextLine();
+		
+		for(int i=0;i<str.length();i++){
+				char c=str.charAt(i);
+		
+		switch(c){
+		case 'a':
+			System.out.println("a");
+			break;
+		case 'e':
+			System.out.println("e");
+			break;
+		case 'i':
+			System.out.println("i");
+			break;
+		case 'o':
+			System.out.println("o");
+			break;
+		case 'u':
+			System.out.println("u");
+			break;
+			
+		}
+					
+		System.out.println(" vowel "+c);
+			
+		}
+		
 
-}
+		
+		
+	}}
+
+
