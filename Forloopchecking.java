960c714b0e2f073748b033534c393e42e507fbbf
muthuvelmurugan75 <muthuvelmurugan75@gmail.com>
2017-04-16 22:40:56 +0530

    Add files via upload

---
+++ Forloopchecking.java
@@ -0,0 +1,37 @@
+package com.work.www;
+
+public class Forloopchecking {
+
+	public static void main(String[] args) {
+
+
+
+for(int i=1;i<5;i++){
+	//System.out.print("*");
+	for(int j=1;j<i;j++){
+		System.out.print("*");
+	}
+	System.out.println("*");
+	
+}
+
+for(int i=5;i>1;i--){
+	//System.out.print("*");
+	for(int j=1;j<i;j++){
+		System.out.print("*");
+	}
+	System.out.println("*");
+	
+}
+
+
+		
+		
+
+		
+	}
+
+		
+	
+
+}
