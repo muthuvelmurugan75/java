9c77a8fd6ee77c408891045341484ac76710dff0
muthuvelmurugan75 <muthuvelmurugan75@gmail.com>
2017-06-06 07:26:40 +0530

    Add files via upload

---
+++ Casting.java
@@ -0,0 +1,11 @@
+package com.payilagam.www;
+
+public class Casting {
+
+	public static void main(String[] args) {
+int i=100;
+byte b=(byte) i;
+System.out.println(b);
+	}
+
+}
--- Factorial.java
+++ Factorial.java
@@ -1,21 +1,21 @@
-package com.practiceprograms.www;
+package com.payilagam.www;
 
 import java.util.Scanner;
 
 public class Factorial {
 
 	public static void main(String[] args) {
-		int sum=1;
-Scanner obj=new Scanner(System.in);
-System.out.println("enter the numer");
-int a=obj.nextInt();
-for(int i=1;i<=a;i++){
-	 sum=sum*i;
-System.out.println(sum);
-}
 
 		
-		
+			int sum=1;
+	Scanner obj=new Scanner(System.in);
+	System.out.println("enter the numer");
+	int a=obj.nextInt();
+	for(int i=1;i<=a;i++){
+		 sum=sum*i;
+	System.out.println(sum);
+	}
+
 	}
 
 }
