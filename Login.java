86a2c07f5615ed48375a9cc0b844cd6334a53125
muthuvelmurugan75 <muthuvelmurugan75@gmail.com>
2017-06-25 22:34:03 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,23 @@
+package com.Contacts.www;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner; // I use scanner because it's command line.
+
+public class Login {
+public void run() throws FileNotFoundException {
+    Scanner scan = new Scanner (new File("the\\dir\\myFile.extension"));
+    Scanner keyboard = new Scanner (System.in);
+    String user = scan.nextLine();
+    String pass = scan.nextLine(); // looks at selected file in scan
+
+    String inpUser = keyboard.nextLine();
+    String inpPass = keyboard.nextLine(); // gets input from user
+
+    if (inpUser.equals(user) && inpPass.equals(pass)) {
+        System.out.print("your login message");
+    } else {
+        System.out.print("your error message");
+    }
+}
+} 
