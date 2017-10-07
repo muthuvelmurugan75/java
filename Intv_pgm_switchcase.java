c74470e1b6cfe38cf6171414b70468bbbed39e2a
muthuvelmurugan75 <muthuvelmurugan75@gmail.com>
2017-10-07 23:13:43 +0530

    Add files via upload

---
+++ Intv_pgm_switchcase.java
@@ -0,0 +1,15 @@
+package com.technique.www;
+
+public class Intv_pgm_switchcase {
+
+	public static void main(String[] args) {
+		 int number=10;  
+		    switch(number){  
+		    case 10: System.out.println("10"); 
+		    case 20: System.out.println("20");  
+		    case 30: System.out.println("30"); 
+		    default:System.out.println("Not in 10, 20 or 30");  
+		    }  
+	}
+
+}
