eb39ec6429668e8297003e9ee29cd057bd4170e4
muthuvelmurugan75 <muthuvelmurugan75@gmail.com>
2017-11-09 21:47:37 +0530

    Add files via upload

---
+++ Pattern_using_while.java
@@ -0,0 +1,18 @@
+package com.tricky.www;
+
+public class Pattern_using_while {
+	public static void main(String[] args) {
+        int i=4,j,l;
+        while(0<i){
+            j=0;
+            while(j<i){
+                l=4-j;
+                System.out.print(l);
+            j++;
+                
+        }
+        System.out.println();
+        i--;
+    }
+    }
+}
