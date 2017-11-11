8f35a2217ebc3181122a77f2bcfa777496f3f9aa
muthuvelmurugan75 <muthuvelmurugan75@gmail.com>
2017-11-11 22:31:16 +0530

    Add files via upload

---
+++ Pascaltriangle_11_11_2017.java
@@ -0,0 +1,29 @@
+package com.tricky.www;
+
+import java.util.Scanner;
+
+public class Pascaltriangle_11_11_2017 {
+	public static void main(String args[])
+	{
+	    int r, i, k, number=1, j;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter Number of Rows : ");
+		r = scan.nextInt();
+		
+		for(i=0;i<r;i++)
+		{
+			for(k=r; k>i; k--)
+			{
+				System.out.print("E");
+			}
+            number = 1;
+			for(j=0;j<=i;j++)
+			{
+				 System.out.print(number+ " ");
+                 number = number * (i - j) / (j + 1);
+			}
+			System.out.println();
+		}
+	}
+}
