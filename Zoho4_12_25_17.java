0faccde0e05bc9051f0fc2243aa9d1f8341053e5
muthuvelmurugan75 <muthuvelmurugan75@gmail.com>
2017-12-25 09:10:49 +0530

    Add files via upload

---
+++ Zoho4_12_25_17.java
@@ -0,0 +1,16 @@
+package com.tricky.www;
+
+public class Zoho4_12_25_17 {
+
+	public static void main(String[] args) {
+String [] str={"alpha","beta","gamma"};
+	String rev[]=new String[str.length];
+	for(int i=str.length-1;i>=0;i--){
+		rev[i]=str[i];
+		System.out.println(rev[i]);
+	}
+	
+}
+
+
+	}
