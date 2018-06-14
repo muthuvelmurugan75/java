a822e8ece100e4e4fe945179b88b36119bdf8cf2
muthuvelmurugan75 <muthuvelmurugan75@gmail.com>
2018-06-14 07:22:44 +0530

    Add files via upload

---
+++ Demo_html.java
@@ -0,0 +1,70 @@
+package tricky;
+
+public class Demo_html {
+	<html>
+	<head>
+	<title> Muthuvel</title>
+
+	</head>
+
+	<style>
+	     body{background-color:pink;}
+	      .1{width:100%; height:600px; border:1px black; }
+		  
+	      .header{width:100%; height:100px; border:1px solid green; }
+		        .h{width:100%; height:100px; }
+
+	      .h1{width:33%; height:90px; border:1px solid yellow; float:left; padding:1px; margin-top:2px;}
+		  
+	    .b{width:100%; height:400px; border:1px solid red; padding:40px; box-sizing:border-box;}
+		  
+		    
+			
+		  
+	</style>
+
+
+	<body>
+	      <div class="1">
+		  
+					  <div class="header">
+					  
+					       <div class="h1"> <h1> <span style="color:red;">Muthuvel top</span></h1> </div>
+					       <div class="h1" >  </div>
+					       <div  class="h1">  </div>
+					  
+					  </div>
+					  
+					  
+					  <div class="b"> 
+					  
+					  <div>
+					  <h1> Muthuvel </h1>
+					  </div>
+					  <button> submit</button>
+					  
+					  
+					  </div>
+		  
+		  
+		  
+		             <div class="h">
+					  
+					       <div class="h1"> <h1> <span style="color:red;">Muthuvel</span></h1> </div>
+					       <div class="h1" >  </div>
+					       <div  class="h1">  </div>
+					  
+					  </div>
+					  
+		  
+		  </div>
+
+
+
+	</body>
+
+
+
+
+	</html>
+}
