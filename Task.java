9adee2e14544cc128913e160d3997d3989b957f1
muthuvelmurugan75 <muthuvelmurugan75@gmail.com>
2019-01-10 21:29:20 +0530

    Add files via upload

---
+++ Task.java
@@ -0,0 +1,18 @@
+package CC;
+
+public class Task {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+int a=11,b=0,c=0;
+if(a>10 && (b=b+5) >0 && (c=c+10)>0 || a<5 && (b=b+25)>0 && (c=c+35)>0){
+	System.out.println("c=="+c);
+	System.out.println("b=="+b);
+
+}
+	}
+
+}
